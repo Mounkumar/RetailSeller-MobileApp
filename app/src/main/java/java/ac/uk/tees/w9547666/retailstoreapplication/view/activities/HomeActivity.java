4720b5d8a237079a55cd4de04916d826064933a4
Mounkumar <49392940+Mounkumar@users.noreply.github.com>
2022-04-21 19:00:23 +0100

    Reusable animation componants for splash screen to be developed - close #6

---
+++ app/src/main/java/java/ac/uk/tees/w9547666/retailstoreapplication/view/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package java.ac.uk.tees.w9547666.retailstoreapplication.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import java.ac.uk.tees.w9547666.retailstoreapplication.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
